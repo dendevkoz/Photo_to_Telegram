d32007ca09c657b997f18f40e65142d6e56594ea
dendevkoz <153610622+dendevkoz@users.noreply.github.com>
2024-01-26 15:50:55 +0300

    Update fetch_nasa.py
    Добавлена проверка каждого HTTP ответа в программе

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -22,6 +22,7 @@ def fetch_nasa_apod(nasa_api_key):
         "api_key": nasa_api_key,
     }
     response = requests.get(nasa_apod_url, params=payload)
+    response.raise_for_status()
     data = response.json()
     directory = "images/nasa_apod"
     os.makedirs(directory, exist_ok=True)
@@ -30,7 +31,10 @@ def fetch_nasa_apod(nasa_api_key):
     for image_number, image_url in enumerate(image):
         extension = str(file_extension(image_url))
         image_path = f"{directory}/nasa_apod{image_number}{extension}"
-        save_all_image(image_url, image_path)  
+        try:
+            save_all_image(image_url, image_path)
+        except requests.exceptions.HTTPError as error:
+            logging.error("Не удалось сохранить изображение с сайта NASA-APOD:\n{0}".format(error))  
 
 
 def fetch_nasa_epic(nasa_api_key):
@@ -40,6 +44,7 @@ def fetch_nasa_epic(nasa_api_key):
         "api_key": nasa_api_key,
     }
     date_response = requests.get(nasa_epic_url, params=payload)
+    date_response.raise_for_status()
     data = date_response.json()
     directory = "images/nasa_epic"
     os.makedirs(directory, exist_ok=True)
@@ -53,11 +58,17 @@ def fetch_nasa_epic(nasa_api_key):
         response = requests.get(f"https://api.nasa.gov/EPIC/archive/natural/{last_date_formatted}/png/{item + '.png'}", params=payload)
         extension = ".png"
         image_path = f"{directory}/nasa_epic_{item[-4:]}{extension}"
-        save_all_image(response.url, image_path)
+        try:
+            save_all_image(response.url, image_path)
+        except requests.exceptions.HTTPError as error:
+            logging.error("Не удалось сохранить изображение с сайта NASA-EPIC:\n{0}".format(error))
 
 
 
 if __name__ == '__main__':
     nasa_api_key = dotenv_values(".env")["NASA_KEY"]
-    fetch_nasa_apod(nasa_api_key)
-    fetch_nasa_epic(nasa_api_key)
+    try:
+        fetch_nasa_epic(nasa_api_key)
+        fetch_nasa_apod(nasa_api_key)
+    except requests.exceptions.HTTPError as error:
+        exit("Невозможно получить данные с сайта NASA EPIC или NASA APOD:\n{0}".format(error))
