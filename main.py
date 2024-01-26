5095917c0702b247ad0b520c4d55f6f5d2e227f6
dendevkoz <153610622+dendevkoz@users.noreply.github.com>
2024-01-26 15:55:20 +0300

    Update main.py
    Изменено имя переменной

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ from dotenv import dotenv_values
 from time import sleep
 
 
-def posting(bot, chat_id):
+def posting(bot, telegram_chat_id):
     while True:
         for root_folder, folder, file in os.walk('images'):
             for filename in file:
                 image_path = f"{root_folder}/{filename}"
                 with open(image_path, "rb") as image_file:
                     photo = image_file.read()
-                bot.send_photo(chat_id=chat_id, photo=photo)
+                bot.send_photo(chat_id=telegram_chat_id, photo=photo)
                 sleep(float(post_time))
 
 
 if __name__ == "__main__":
     telegram_token = dotenv_values(".env")["TELEGRAM_TOKEN"]
-    chat_id = dotenv_values(".env")["CHAT_ID"]
+    telegram_chat_id = dotenv_values(".env")["CHAT_ID"]
     bot = telegram.Bot(token=telegram_token)
     post_time = dotenv_values(".env")["POST_TIME"]
-    posting(bot, chat_id)
+    posting(bot, telegram_chat_id)
