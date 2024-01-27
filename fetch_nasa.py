7b30af71121f8329e0678f7c23f941f1ce032c0d
dendevkoz <153610622+dendevkoz@users.noreply.github.com>
2024-01-27 03:54:53 +0300

    Update fetch_nasa.py
    Изменены названия переменных
    Изменено создание папки для сохранения
    Добавлен ввод количества изображений для загрузки
    Изменен принцип создания имени изображения, путем добавления даты в секундах

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -2,23 +2,16 @@ import os
 import requests
 import datetime
 from dotenv import dotenv_values
-from save_image import save_all_image
-from urllib.parse import urlsplit, unquote
-from os import path
-
-
-def file_extension(link):
-  link = unquote(link)
-  link_split = urlsplit(link)
-  extension = path.splitext(link_split.path)[1]
-  return extension
+from save_image import save_all_image, create_directory, extension_file
+import logging
   
 
 def fetch_nasa_apod(nasa_api_key):
     nasa_apod_url = f"https://api.nasa.gov/planetary/apod"
-    image=[]
+    urls_images=[]
+    count = int(input("Сколько картинок скачать?(Число от 1 до 50): "))
     payload = {
-        "count": 5,
+        "count": count,
         "api_key": nasa_api_key,
     }
     response = requests.get(nasa_apod_url, params=payload)
@@ -26,11 +19,11 @@ def fetch_nasa_apod(nasa_api_key):
     data = response.json()
     directory = "images/nasa_apod"
     os.makedirs(directory, exist_ok=True)
-    for item in data:
-        image.append(item["url"])
+    for description in data:
+        urls_images.append(description["url"])
     for image_number, image_url in enumerate(image):
         extension = str(file_extension(image_url))
-        image_path = f"{directory}/nasa_apod{image_number}{extension}"
+        image_path = os.path.join(create_directory(), f"nasa_apod{image_number}{extension}")
         try:
             save_all_image(image_url, image_path)
         except requests.exceptions.HTTPError as error:
@@ -39,7 +32,7 @@ def fetch_nasa_apod(nasa_api_key):
 
 def fetch_nasa_epic(nasa_api_key):
     nasa_epic_url = f"https://api.nasa.gov/EPIC/api/natural/all"
-    image = []
+    picture_names = []
     payload = {
         "api_key": nasa_api_key,
     }
@@ -52,12 +45,13 @@ def fetch_nasa_epic(nasa_api_key):
     last_date_formatted = last_date.strftime("%Y/%m/%d")
     last_date_respone = requests.get(f"https://api.nasa.gov/EPIC/api/natural/date/{last_date}",     params=payload)
     last_date_image = last_date_respone.json()
-    for item in last_date_image:
-        image.append(item["image"])
-    for item in image:
-        response = requests.get(f"https://api.nasa.gov/EPIC/archive/natural/{last_date_formatted}/png/{item + '.png'}", params=payload)
+    for description in last_date_image:
+        picture_names.append(description["image"])
+    for name in picture_names:
+        response = requests.get(f"https://api.nasa.gov/EPIC/archive/natural/{last_date_formatted}/png/{name}.png", params=payload)
         extension = ".png"
-        image_path = f"{directory}/nasa_epic_{item[-4:]}{extension}"
+        picture_date = datetime.datetime.now().timestamp()
+        image_path = os.path.join(create_directory(), f"nasa_epic_{int(picture_date)}{extension}")
         try:
             save_all_image(response.url, image_path)
         except requests.exceptions.HTTPError as error:
