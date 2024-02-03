59075d2817455adb0f245864b067fafa17051057
dendevkoz <153610622+dendevkoz@users.noreply.github.com>
2024-02-04 02:23:26 +0300

    Исправлено переподключение после обрыва сети

--- main.py
+++ main.py
@@ -2,25 +2,26 @@ import telegram
 import os
 from dotenv import dotenv_values
 from time import sleep
+from telegram.error import NetworkError
 
 
-def send_to_bot(bot, telegram_chat_id):
-    try:
-        while True:
-            for root_folder, folder, file in os.walk('images'):
-                for file_name in file:
-                    image_path = f"{root_folder}/{file_name}"
-                    with open(image_path, "rb") as image_file:
-                        photo = image_file.read()
+def send_to_bot():
+    while True:
+        for root_folder, folder, file in os.walk('images'):
+            for file_name in file:
+                image_path = f"{root_folder}/{file_name}"
+                with open(image_path, "rb") as image_file:
+                    photo = image_file.read()
+                try:
+                    bot = telegram.Bot(token=telegram_token)
                     bot.send_photo(chat_id=telegram_chat_id, photo=photo)
                     sleep(14400)
-    except telegram.error.TimedOut:
-        print(f"Ошибка подключения. Повторная попытка через 5 секунд...")
-        telegram.error.RetryAfter(5)
+                except NetworkError:
+                    print("Ошибка подключения. Повторная попытка через 10 секунд...")
+                    sleep(10)
 
 
 if __name__ == "__main__":
     telegram_token = dotenv_values(".env")["TELEGRAM_TOKEN"]
     telegram_chat_id = dotenv_values(".env")["CHAT_ID"]
-    bot = telegram.Bot(token=telegram_token)
-    send_to_bot(bot, telegram_chat_id)
+    send_to_bot()
