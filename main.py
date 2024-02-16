515b76ca6bbf61fa5b92ea07b1c734831a84a570
dendevkoz <153610622+dendevkoz@users.noreply.github.com>
2024-02-16 20:22:54 +0300

    Добавлен выбор папки из которой загружать картинки в Telegram

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import os
 from dotenv import dotenv_values
 from time import sleep
 from telegram.error import NetworkError
+import argparse
 
 
 def send_to_bot():
     while True:
-        for root_folder, folder, file in os.walk('images'):
+        for root_folder, folder, file in os.walk(dir_name):
             for file_name in file:
                 image_path = os.path.join(f"{root_folder}", f"{file_name}")
                 with open(image_path, "rb") as image_file:
@@ -24,4 +25,16 @@ def send_to_bot():
 if __name__ == "__main__":
     telegram_token = dotenv_values(".env")["TELEGRAM_TOKEN"]
     telegram_chat_id = dotenv_values(".env")["TG_CHAT_ID"]
+    parser = argparse.ArgumentParser(
+        description="Загрузка изображений в Телеграм"
+    )
+    parser.add_argument(
+        "-d",
+        "--dir_name",
+        type=str,
+        help="Имя директории откуда будут загружаться картинки (По умолчанию : images)",
+        default="images",
+    )
+    args = parser.parse_args()
+    dir_name = args.dir_name
     send_to_bot()
