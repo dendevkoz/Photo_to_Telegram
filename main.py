abc8f32a592be319ec9ab95db26654df743b8dda
dendevkoz <153610622+dendevkoz@users.noreply.github.com>
2024-03-17 18:57:56 +0300

    Убран лишний отступ

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import argparse
 from help_functions import posted_all, posted_one, posted_random_image
 
 
-
 def send_to_bot(dir_name, time_sleep, image_path):
     try:
         bot = telegram.Bot(token=telegram_token)
