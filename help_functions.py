2616248574e1f4c788e4ef6652b66e1cadbf1a9f
Den <153610622+dendevkoz@users.noreply.github.com>
2024-04-06 10:43:29 +0300

    Исправлен отступ

--- help_functions.py
+++ help_functions.py
@@ -6,7 +6,6 @@ from time import sleep
 import random
 
 
-
 def save_all_image(image_url, image_path, headers=None, params=None):
     response = requests.get(image_url, headers=headers, params=params)
     response.raise_for_status()
