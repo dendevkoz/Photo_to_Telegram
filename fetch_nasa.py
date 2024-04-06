582fc24a8ba2cc5b0b73dcde1f06681aa5ca2769
Den <153610622+dendevkoz@users.noreply.github.com>
2024-04-06 21:14:59 +0300

    Изменено название переменной

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -34,7 +34,7 @@ def fetch_nasa_epic(nasa_api_key, dir_name):
     latest_date_formatted = latest_date.strftime("%Y/%m/%d")
     nasa_latest_date_url = f"https://api.nasa.gov/EPIC/api/natural/date/{latest_date}"
     latest_date_images = get_response(nasa_latest_date_url, payload)
-    picture_names = [description["image"] for description in latest_date_images]
+    pictures_names = [description["image"] for description in latest_date_images]
     for name in picture_names:
         image_url = f"https://api.nasa.gov/EPIC/archive/natural/{latest_date_formatted}/png/{name}.png"
         extension = ".png"
